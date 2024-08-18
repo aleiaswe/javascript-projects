14e1e6a991f31f50691dea9225ec5064c1b80bd5
Aleia <aleiaparker434@gmail.com>
2024-08-18 02:24:51 -0500

    Objects: Studio 1

--- objects-and-math/studio/ObjectsStudio01.js
+++ objects-and-math/studio/ObjectsStudio01.js
@@ -1,11 +1,41 @@
 // Code your selectRandomEntry function here:
 
+const selectRandomEntry = (arr) => { 
+  // get a random number
+  // round down 
+ return arr[Math.floor(Math.random()*arr.length)]
+};
 
 // Code your buildCrewArray function here:
+
+const buildCrewArray = (ids, candidates) => { 
+  const crew = [];
+  // loop through ids 
+  // loop through candidates
+  // check id against each candidate 
+  // if a match, add to crew array
+  for (let i = 0; i < ids.length; i++) { 
+    for (let j = 0; j < candidates.length; j++) { 
+      if (ids[i] === candidates[j].astronautID) { 
+        crew.push(candidates[j]);
+      }
+    }
+  }
+  return crew;
+};
 
 
 let idNumbers = [291, 414, 503, 599, 796, 890];
 
+const chosenNumbers = [];
+
+while (chosenNumbers.length < 3) { 
+  
+  const chosenNumber = selectRandomEntry(idNumbers);
+  if (!chosenNumbers.includes(chosenNumbers)) { 
+    chosenNumbers.push(chosenNumber)
+  }
+}
 // Here are the candidates and the 'animals' array:
 let candidateA = {
   'name':'Gordon Shumway',
@@ -53,3 +83,8 @@ let candidateF = {
 let animals = [candidateA,candidateB,candidateC,candidateD,candidateE,candidateF];
 
 // Code your template literal and console.log statements:
+console.log(selectRandomEntry(idNumbers));
+console.log(chosenNumbers);
+
+const spaceAnimals = buildCrewArray(chosenNumbers, animals);
+console.log(`${spaceAnimals[0].name},${spaceAnimals[1].name}, and ${spaceAnimals[2].name} are going to space!`);
