553f04e3fa7c7cca00b9aff5c2286276fd043c1a
Aleia <aleiaparker434@gmail.com>
2024-08-18 02:26:25 -0500

    Objects: Studio 2

--- objects-and-math/studio/ObjectsStudio02.js
+++ objects-and-math/studio/ObjectsStudio02.js
@@ -1,14 +1,40 @@
 // Code your orbitCircumference function here:
-
+//Define a function that returns the circumference (C = 2πr) of the orbit. Round the circumference to an integer.
+const orbitCircumference = (radius) => { 
+ return Math.round(2 * Math.PI * radius);
+}
+console.log(orbitCircumference(12));
 
 // Code your missionDuration function here:
+/*Define the missionDuration function to take three parameters - the number of orbits completed, the orbit radius, 
+and the orbital speed. Set the default radius to 2000 km and the default orbital speed to 28000 km/hr. */
+/*Calculate how long it will take our animals to complete a certain number of orbits (time = distance/speed).
+ Round the answer to 2 decimal places, then return the result. For example, with the default radius and speed, 
+ 5 orbits will take about 2.24 hours. */
 
+ const missionDuration = (numOrbits, orbRad=2000, orbSpeed=28000) => { 
+  const time = (numOrbits * (orbitCircumference(orbRad))) / orbSpeed
+  return Math.round(time * 100) / 100;
+}
+
+console.log(missionDuration(5));
 
 // Copy/paste your selectRandomEntry function here:
-
+const selectRandomEntry = (arr) => { 
+  // get a random number
+  // round down 
+ return arr[Math.floor(Math.random()*arr.length)]
+};
 
 // Code your oxygenExpended function here:
-
+const oxygenExpended = (crewMember) => { 
+  //get missionDuration
+  //get o2 per hour 
+  //round to 3 decimal places
+  const duration = missionDuration(3);
+  const o2Used = Math.round(crewMember.o2Used(duration) * 1000) /1000;
+  return `${crewMember.name} will perform the spacewalk, which will last ${duration} hours and require ${o2Used}kg of oxygen.`
+}
 
 // Candidate data & crew array.
 let candidateA = {
@@ -54,5 +80,13 @@ let candidateA = {
    'astronautID':890
  };
  
- let crew = [candidateA,candidateC,candidateE];
+
+ let animals = [candidateA,candidateB,candidateC,candidateD,candidateE,candidateF];
  
+ //let crew = [candidateA,candidateC,candidateE];
+ //array above wouldn't be completely random
+
+ 
+ let walker = selectRandomEntry(animals)
+const o2Statement = oxygenExpended(walker);
+console.log(o2Statement);
